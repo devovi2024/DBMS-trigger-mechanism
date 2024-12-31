484003c3672c90d9e84ecc8ca4755b94d0500aed
devovi2024 <arfanhosenovi@gmail.com>
2024-12-31 17:48:09 +0600

    insert value

---
+++ sql/Step 2 INSERT INTO statement.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (name, passion, age, address)
+VALUES ('Rahim', 'Coding', 25, 'Dhaka');
+
+INSERT INTO person (name, passion, age, address)
+VALUES ('Karim', 'Designing', 30, 'Chittagong');
